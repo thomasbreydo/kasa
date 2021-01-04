31362eed4c087ca5fbb2df89a29e2ad9a2f38180
Thomas Breydo <tbreydo@gmail.com>
2021-01-04 11:01:41 -0500

    Fix typo in help string

--- addflag/addflag.go
+++ addflag/addflag.go
@@ -3,6 +3,6 @@ package addflag
 import "github.com/spf13/cobra"
 
 func IP(p *string, command *cobra.Command) {
-	command.PersistentFlags().StringVar(p, "ip", "", "smartdevice IP or hostname")
+	command.PersistentFlags().StringVar(p, "ip", "", "device IP or hostname")
 	_ = command.MarkPersistentFlagRequired("ip")
 }
